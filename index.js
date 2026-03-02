2a3cc443aca99e7cb03e3a7a7ab7fca136c70879
koddza <shemagister224@gmail.com>
2026-03-02 20:27:58 -0300

    agregando servidor express

--- index.js
+++ index.js
@@ -55,3 +55,14 @@ client.on("messageCreate", (message) => {
 });
 
 client.login(TOKEN);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot activo");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor web activo");
+});S
